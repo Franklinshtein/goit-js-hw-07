623b3d031e22b66ed1d102affa036f807fba8183
Franklinshtein <98875248+Franklinshtein@users.noreply.github.com>
2023-04-01 19:37:17 +0200

    home-work

---
+++ js/01-gallery.js
@@ -0,0 +1,50 @@
+import { galleryItems } from "./gallery-items.js";
+
+// знаходження галереї на сторінці
+const gallery = document.querySelector(".gallery");
+
+// створення розмітки галереї
+const createGalleryMarkup = (items) =>
+  items
+    .map(
+      ({ preview, original, description }) => `
+      <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" data-source="${original}">
+        </a>
+      </div>
+    `
+    )
+    .join("");
+
+// додавання створеної розмітки до галереї
+gallery.innerHTML = createGalleryMarkup(galleryItems);
+
+// додавання слухача подій до галереї
+gallery.addEventListener("click", (event) => {
+  event.preventDefault(); // відміна поведінки за замовчуванням
+
+  const { nodeName, dataset: { source } = {} } = event.target;
+
+  // перевірка чи клік був на зображенні
+  if (nodeName !== "IMG" || !source) return;
+
+  // створення шаблону випадаючого зображення
+  const instance = basicLightbox.create(`<img src="${source}">`);
+
+  // показ шаблону зображення
+  instance.show();
+
+  // додавання слухача подій для закриття шаблону
+  document.addEventListener("keydown", closeModal);
+
+  // закриття шаблону
+  function closeModal({ key }) {
+    if (key === "Escape") {
+      instance.close();
+      document.removeEventListener("keydown", closeModal);
+    }
+  }
+});
+
+console.log(galleryItems);
---
+++ js/02-lightbox.js
@@ -0,0 +1,30 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+// знаходження галереї на сторінці
+const gallery = document.querySelector(".gallery");
+
+// створення розмітки галереї
+const createGalleryMarkup = (items) =>
+  items
+    .map(
+      ({ preview, original, description }) => `
+      <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" data-source="${original}">
+        </a>
+      </div>
+    `
+    )
+    .join("");
+
+// додавання створеної розмітки до галереї
+gallery.innerHTML = createGalleryMarkup(galleryItems);
+
+new SimpleLightbox(".gallery a", {
+  caption: true,
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+console.log(galleryItems);
